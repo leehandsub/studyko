46b720ad10054469aa56c8bd4f81718753981faf
leehandsub <gustjq7027@naver.com>
2021-04-11 19:28:42 +0900

    test

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,71 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+val num_of_rows = 10
+val page_no = 1
+val data_type = "JSON"
+val base_time = 1100
+val base_data = 20200808
+val nx = "55"
+val ny = "127"
+
+data class WEATHER (
+    val response : RESPONSE
+)
+data class RESPONSE (
+    val header : HEADER,
+    val body : BODY
+)
+data class HEADER(
+    val resultCode : Int,
+    val resultMsg : String
+)
+data class BODY(
+    val dataType : String,
+    val items : ITEMS
+)
+data class ITEMS(
+    val item : List<ITEM>
+)
+data class ITEM(
+    val baseData : Int,
+    val baseTime : Int,
+    val category : String
+)
+
+interface WeatherInterface {
+    @GET("getVilageFcst?serviceKey=yZXZgbjUm0xIw25yGLGRz8qhaNvRtwr%2BBUDx8ng795KMf09sW4trkmJ8l0skUrE%2F5NS0qVZOReX1vTVvx%2BWdzQ%3D%3D")
+    fun GetWeather(
+        @Query("dataType") data_type  : String,
+        @Query("numOfRows") num_of_rows : Int,
+        @Query("pageNo") page_no : Int,
+        @Query("base_date") base_date : Int,
+        @Query("base_time") base_time : Int,
+        @Query("nx") nx : String,
+        @Query("ny") ny : String
+    ): Call<WEATHER>
+}
+
+
+private val retrofit = Retrofit.Builder()
+    .baseUrl("http://apis.data.go.kr/1360000/VilageFcstInfoService/") // 마지막 / 반드시 들어가야 함
+    .addConverterFactory(GsonConverterFactory.create()) // converter 지정
+    .build() // retrofit 객체 생성
+
+
+object ApiObject {
+    val retrofitService: WeatherInterface by lazy {
+        retrofit.create(WeatherInterface::class.java)
+    }
+}
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,14 +74,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //val btn_event = findViewById<Button>(R.id.button)
-
-
-        button.setOnClickListener{
-            edit.setText("")
-            Toast.makeText(this,edit.text.toString(),Toast.LENGTH_SHORT).show()//토스트 보여주기
-            textView.setText(edit.text.toString())
-        }
-
+        val call = ApiObject.retrofitService.GetWeather(data_type, num_of_rows, page_no, base_data, base_time, nx, ny)
+        call.enqueue(object : retrofit2.Callback<WEATHER>{
+            override fun onResponse(call: Call<WEATHER>, response: Response<WEATHER>) {
+                if (response.isSuccessful){
+                    Log.d("api", response.body().toString())
+                    Log.d("api", response.body()!!.response.body.items.item.toString())
+                    Log.d("api", response.body()!!.response.body.items.item[0].category)
+                }
+            }
+            override fun onFailure(call: Call<WEATHER>, t: Throwable) {
+                Log.d("api fail : ", t.message)
+            }
+        })
     }
 }
