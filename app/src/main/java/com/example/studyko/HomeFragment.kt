d49f28f07e352d05cf54c197a4ba6ab90c75eda2
leehandsub <gustjq7027@naver.com>
2022-05-27 01:23:04 +0900

    section 5
    splash 화면 추가하는방법, fragment 맛맛보기

---
+++ app/src/main/java/com/example/studyko/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.example.studyko
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button = view.findViewById<Button>(R.id.btnEnterProductDetail)
+        button.setOnClickListener {
+            val transaction = parentFragmentManager.beginTransaction()
+            transaction.add(R.id.containerMain,ProductDetailFragment())
+            transaction.commit()
+        }
+    }
+}
