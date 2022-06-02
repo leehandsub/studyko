79df900fc75c58a951d98491519d8158db09bfa9
leehandsub <gustjq7027@naver.com>
2022-06-02 23:39:39 +0900

    section 6
    네비게이션 화면 추가
    그림 추가

--- app/src/main/java/com/example/studyko/MainActivity.kt
+++ app/src/main/java/com/example/studyko/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.studyko
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigationMain)
+        bottomNavigationView.itemIconTintList = null
     }
 }
