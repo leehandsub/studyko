3a93279d43ff3933fd669e372eac18732b008b42
leehandsub <gustjq7027@naver.com>
2022-06-03 23:55:41 +0900

    section 7
    json 파일 가져오기
    viewpager와 indicator만들기

--- app/src/main/java/com/example/studyko/HomeFragment.kt
+++ app/src/main/java/com/example/studyko/HomeFragment.kt
@@ -1,11 +1,18 @@
 package com.example.studyko
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.google.gson.Gson
 
 class HomeFragment : Fragment() {
 
@@ -19,11 +26,42 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val button = view.findViewById<Button>(R.id.btnEnterProductDetail)
-        button.setOnClickListener {
-            val transaction = parentFragmentManager.beginTransaction()
-            transaction.add(R.id.containerMain,ProductDetailFragment())
-            transaction.commit()
+
+        val toolbarTitle = view.findViewById<TextView>(R.id.toolbarHomeTitle)
+        val toolbarIcon = view.findViewById<ImageView>(R.id.toolbarHomeIcon)
+        val viewpager = view.findViewById<ViewPager2>(R.id.viewpagerHomeBanner)
+        val viewpagerIndicator = view.findViewById<TabLayout>(R.id.viewpagerHomeBannerIndicator)
+
+
+
+        val assetLoader = AssetLoader()
+        val homeJsonString = assetLoader.getJsonString(requireContext(), "home.json")
+        Log.d("homeData", homeJsonString ?: "")
+
+        if (!homeJsonString.isNullOrEmpty()) {
+            val gson = Gson()
+            val homeData = gson.fromJson(homeJsonString, HomeData::class.java)
+
+            toolbarTitle.text = homeData.title.text
+            GlideApp.with(this)
+                .load(homeData.title.iconUrl)
+                .into(toolbarIcon)
+
+            viewpager.adapter = HomeBannerAdapter().apply {
+                submitList(homeData.topBanners)
+            }
+            val pageWidth = resources.getDimension(R.dimen.viewpager_item_width)
+            val pageMargin = resources.getDimension(R.dimen.viewpager_item_margin)
+            val screenWidth = resources.displayMetrics.widthPixels
+            val offset = screenWidth - pageWidth - pageMargin
+
+            viewpager.offscreenPageLimit = 3
+            viewpager.setPageTransformer { page, position ->
+                page.translationX = position * -offset
+            }
+            TabLayoutMediator(viewpagerIndicator, viewpager) { tab, position ->
+
+            }.attach()
         }
     }
 }
--- app/src/main/java/com/example/studyko/MainActivity.kt
+++ app/src/main/java/com/example/studyko/MainActivity.kt
@@ -2,14 +2,22 @@ package com.example.studyko
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigationMain)
         bottomNavigationView.itemIconTintList = null
+
+        val navController = supportFragmentManager.findFragmentById(R.id.containerMain)?.findNavController()
+        navController?.let {
+            bottomNavigationView.setupWithNavController(it)
+        }
     }
 }
